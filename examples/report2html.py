447b2c1e232fdf9265cd81504f865c2a5aa507d6
Suchakra Sharma <suchakra@shiftleft.io>
2020-03-28 16:55:33 -0700

    Licence report summary only

--- examples/report2html.py
+++ examples/report2html.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append("..")
 import reporter.sarif as sarif
 import reporter.grafeas as grafeas
+import reporter.licence as licence
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -14,11 +15,15 @@ if args.input:
     # Quick condition for depscan testing
     if "depscan" in args.input:
         report_data = grafeas.parse(args.input)
-    else:
+    elif "sarif" in args.input:
         report_data = sarif.parse(args.input)
+    elif "license" in args.input:
+        report_data = licence.parse(args.input)
 if args.output:
     with open(args.output, mode="w", encoding="utf-8") as f:
         if "depscan" in args.input:
             grafeas.render_html(report_data, f.name)
-        else:
+        elif "sarif" in args.input:
             sarif.render_html(report_data, f.name)
+        elif "license" in args.input:
+            licence.render_html(report_data, f.name)
