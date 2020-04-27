4403e6d8845e79e103c27be129a6cb8cd565f17b
Chaitanya K <kamthamc@users.noreply.github.com>
2020-04-27 14:41:18 -0700

    fix: fix block type and margin to chips in the table (#24)
    * fix: fix block type and margin to chips in the table
    
    * fix: bump version

--- reporter/utils.py
+++ reporter/utils.py
@@ -7,13 +7,13 @@ def auto_text_highlight(text):
     :param text:
     :return:
     """
-    text = text.replace("In class", "<span class='bg-light chip'>Class</span>")
-    text = text.replace("In method", "<span class='bg-light chip'>Method</span>")
-    text = text.replace("At ", "<span class='bg-light chip'>File</span>")
-    text = text.replace("line ", "<span class='bg-light chip'>line</span>")
-    text = text.replace("Commit:", "<span class='bg-light chip'>Commit</span>")
-    text = text.replace("Line:", "<span class='bg-light chip'>Line</span>")
-    text = text.replace("Message:", "<span class='bg-light chip'>Message</span>")
+    text = text.replace("In class", "<span class='bg-light chip d-inline-block mb-1'>Class</span>")
+    text = text.replace("In method", "<span class='bg-light chip d-inline-block mb-1'>Method</span>")
+    text = text.replace("At ", "<span class='bg-light chip d-inline-block mb-1'>File</span>")
+    text = text.replace("line ", "<span class='bg-light chip d-inline-block mb-1'>line</span>")
+    text = text.replace("Commit:", "<span class='bg-light chip d-inline-block mb-1'>Commit</span>")
+    text = text.replace("Line:", "<span class='bg-light chip d-inline-block mb-1'>Line</span>")
+    text = text.replace("Message:", "<span class='bg-light chip d-inline-block mb-1'>Message</span>")
     text = text.replace("\n", "<br/>")
     # Boldify verbs
     for v in VERBS_LIST:
