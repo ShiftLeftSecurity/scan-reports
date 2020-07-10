eb287720945d3e0aa6ac4217a749bbec87eee8b0
Prabhu Subramanian <prabhu@shiftleft.io>
2020-07-10 23:04:38 +0100

    Fix version (#29)
    * Fix version
    
    * Fix version

--- test/test_grafeas.py
+++ test/test_grafeas.py
@@ -9,6 +9,11 @@ def test_gr_file():
     return Path(__file__).parent / "data" / "depscan-report-java.json"
 
 
+@pytest.fixture
+def test_node_gr_file():
+    return Path(__file__).parent / "data" / "depscan-report-nodejs.json"
+
+
 def test_parse(test_gr_file):
     report_data = parse(test_gr_file)
     assert report_data
@@ -20,3 +25,10 @@ def test_render(test_gr_file):
         html_content = render_html(report_data, hfile.name)
         assert "{{" not in html_content
         assert "UNSPECIFIED" in html_content
+
+
+def test_nodejs_render(test_node_gr_file):
+    report_data = parse(test_node_gr_file)
+    with tempfile.NamedTemporaryFile(mode="w", encoding="utf-8", delete=True) as hfile:
+        html_content = render_html(report_data, hfile.name)
+        assert "Fix Version" in html_content
