0a154479c50c18f8da548ee0efc1699d81706957
Prabhu Subramanian <prabhu@shiftleft.io>
2021-07-23 17:09:02 +0100

    GitHub issue comment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shiftleft-scan-reports",
-    version="1.3.4",
+    version="1.4.0",
     author="Team ShiftLeft",
     author_email="hello@shiftleft.io",
     description="Library for producing SARIF & html reports from ShiftLeft or AppThreat scan results",
